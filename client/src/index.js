df2315b3296f01a9cfdc9a81c17a64d7fc178867
yekyuhwang <4leaf.hyk@gmail.com>
2021-08-24 13:18:20 +0900

    a

---
+++ client/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Noto Sans KR", serif',
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
